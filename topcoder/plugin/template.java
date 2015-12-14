e2aa5b98f0ac916f68cb98efe1847651532dd2ed
Lev Neiman <lneiman@lneiman-ld1.linkedin.biz>
2015-12-14 15:19:54 -0800

    Adding java template

---
+++ topcoder/plugin/template.java
@@ -0,0 +1,16 @@
+// $BEGINCUT$
+/**
+ $PROBLEMDESC$
+ **/
+// $ENDCUT$
+import java.io.PrintStream;
+import java.util.*;
+
+
+public class $CLASSNAME$ {
+  public static final PrintStream out = System.out;
+
+  public $RC$ $METHODNAME$($METHODPARMS$) {
+
+  }
+}
