dad8f4bcd0cd64654c24d042417ed2d91b773681
lan17 <lev.neiman@gmail.com>
2017-12-05 12:43:08 -0800

    better solution

--- advent_of_code/day_1.hs
+++ advent_of_code/day_1.hs
@@ -1,10 +1,14 @@
 import Data.Char
 
+{--
 captcha' _ [] = 0
 captcha' h [x] = if h == x then digitToInt h else 0
 captcha' h (x:y:xs) = if x == y then (digitToInt x) + r else 0 + r
     where r = captcha' h (y:xs)
 captcha xx@(x:xs) = captcha' x xx
+--}
+
+captcha s = sum $ map (\(x,y) -> if x == y then digitToInt x else 0) $ zip s $ tail s ++ [head s]
 
 
 
--- haskell/baby.hs
+++ haskell/baby.hs
@@ -4,5 +4,15 @@ fac n = n * fac(n-1)
 powlist 0 = [1]
 powlist n = 2 * head( s ) : s where s = powlist(n-1)
 plf n = [2**x | x <- [1..n]]
+digits :: Integer -> [Integer]
+digits 0 = []
+digits x = (x `mod` 10) : (digits $ x `div` 10)
+
+isLucky n = firstHalf == secondHalf where
+        digitList = digits n
+        nn = length (digitList) `div` 2
+        firstHalf = sum (take nn digitList)
+        secondHalf = sum (drop nn digitList)
+
 
 main = print $ fac 5
