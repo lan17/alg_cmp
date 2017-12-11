d3a1b10e555713cf21107db3ed335e8fd5952dbd
Lev Alexandrovich Neiman <lev.neiman@gmail.com>
2017-12-10 16:18:21 -0800

    baby

--- haskell/baby.hs
+++ haskell/baby.hs
@@ -15,4 +15,35 @@ isLucky n = firstHalf == secondHalf where
         secondHalf = sum (drop nn digitList)
 
 
-main = print $ fac 5
+initials :: String -> String -> String
+initials (f:_) (l:_)= [f] ++ ", " ++ [l]
+
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted = quicksort [a | a <- xs, a > x]
+    in  smallerSorted ++ [x] ++ biggerSorted
+
+{-|
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge x [] = x
+merge [] y = y
+merge xx@(x:xs) yy@(y:ys) = if x < y then x:(merge xs yy) else y:(merge ys xx)
+
+mergesort :: (Ord a) => [a] -> [a]
+mergesort xs = merge
+-}
+
+sortByHeight a =
+        let people = quicksort $ filter (/= -1) a
+            replacePeople [] _ = []
+            replacePeople _ [] = []
+            replacePeople (x:xs) yy@(y:ys) = if x == -1 then x : (replacePeople xs yy) else y : (replacePeople xs ys)
+        in replacePeople a people
+
+main = do
+  print $ fac 5
+  print (initials "Lev" "Neiman")
+  print (quicksort "FUCK YOU AND SHIT")
+  print $ sortByHeight [-1, 150, 190, 170, -1, -1, 160, 180]
+
