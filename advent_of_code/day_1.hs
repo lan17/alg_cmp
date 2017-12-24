7b1c721ff92acc1d644806f7f55908940c0a3a31
Lev Alexandrovich Neiman <lev.neiman@gmail.com>
2017-12-24 14:38:55 -0800

    frege

--- advent_of_code/day_1.hs
+++ advent_of_code/day_1.hs
@@ -1,13 +1,5 @@
 import Data.Char
 
-{--
-captcha' _ [] = 0
-captcha' h [x] = if h == x then digitToInt h else 0
-captcha' h (x:y:xs) = if x == y then (digitToInt x) + r else 0 + r
-    where r = captcha' h (y:xs)
-captcha xx@(x:xs) = captcha' x xx
---}
-
 captcha :: Int -> [Char] -> Int
 captcha n s = sum $ map (\(x,y) -> if x == y then digitToInt x else 0) $ zip s ss
     where
