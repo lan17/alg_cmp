ee742021039f11a9c61b0154ba4bdda3e8ab9f23
Lev Alexandrovich Neiman <lev.neiman@gmail.com>
2018-03-24 14:15:49 -0700

    GCJ 2016, Qualification C, Haskell

---
+++ GCJ/haskell/2016/QualificationB.hs
@@ -0,0 +1,53 @@
+import Data.List.Split
+import Data.List
+import Numeric (showHex, showIntAtBase)
+import Data.Char (intToDigit)
+
+import GCJ
+
+-- Convert an integer to base 2 String
+intAsBinary :: Integer -> [Char]
+intAsBinary x = showIntAtBase 2 intToDigit x ""
+
+digitToInt :: Char -> Integer
+digitToInt '1' = 1
+digitToInt '0' = 0
+
+-- map from jam coin as list of bits to its representations in different radices.
+toNumbersFromDigits :: [Integer] -> [Integer]
+toNumbersFromDigits digits = map toNumbers' [2..10]
+    where toNumbers' base = foldr (\(pos, digit) accum -> accum + digit * base ^ pos) 0 (zip [0..] $ reverse digits)
+
+toNumbersFromString :: [Char] -> [Integer]
+toNumbersFromString str = toNumbersFromDigits $ map digitToInt str
+
+toNumbersFromMask :: Integer -> [Integer]
+toNumbersFromMask mask = toNumbersFromString $ intAsBinary mask
+
+divisors = take 1000 primes
+
+-- Generate lazy list of Jam Coins of length N
+goodJamCoins n = [x | x <- [start, (start + 2)..], checkJamCoinBases $ toNumbersFromMask x]
+    where start = 2 ^ (n - 1) + 1
+          checkJamCoinBases nums = foldr (&&) True $ map isNotPrimeNumber nums
+          isNotPrimeNumber x = any (\divisor -> x /= divisor && x `mod` divisor == 0) divisors
+
+-- Map jam coin value in different base to the first divisor in that base.
+getJamCoinDivisors :: [Integer] -> [Integer]
+getJamCoinDivisors jamCoinBases = map (\b -> head [divisor | divisor <- divisors, b `mod` divisor == 0]) jamCoinBases
+
+-- Solve individual case given N and J and output a String
+getJamCoins :: Int -> Int -> [Char]
+getJamCoins n j = intercalate "\n" $ map outputJamCoinLine (take j $ goodJamCoins n)
+    where
+        outputJamCoinLine jamCoin = intercalate " " $ [intAsBinary jamCoin] ++ jamCoinDivisorsString
+            where jamCoinDivisorsString = map show $ jamCoinDivisors
+                  jamCoinDivisors = getJamCoinDivisors $ toNumbersFromMask jamCoin
+
+solve :: [Char] -> [Char]
+solve caseInput = "\n" ++ getJamCoins n j
+    where n = read $ inputSplit !! 0 :: Int
+          j = read $ inputSplit !! 1 :: Int
+          inputSplit = splitOn " " caseInput
+
+main = gcjMain solve
--- GCJ/haskell/GCJ.hs
+++ GCJ/haskell/GCJ.hs
@@ -1,7 +1,10 @@
-module GCJ where
+module GCJ (gcjMain, primes) where
 
 import Data.List
 import System.Environment
+import Criterion.Measurement
+
+-- I/O helper functions
 
 -- Process entire GCJ style problem input and produce GCJ style output.
 solutionsToString :: ([Char] -> [Char]) -> [Char] -> [Char]
@@ -12,9 +15,30 @@ solutionsToString solveFn content = intercalate "\n" solutions
         cases = lines content
 
 gcjMain :: ([Char] -> [Char]) -> IO ()
-gcjMain solve = do
+gcjMain solveFn = do
     args <- getArgs
     let inputFileName = args !! 0
     let outputFileName = inputFileName ++ ".out"
     content <- readFile $ args !! 0
-    writeFile outputFileName $ solutionsToString solve content
+
+    startTime <- getCPUTime
+
+    writeFile outputFileName $ solutionsToString solveFn content
+
+    endTime <- getCPUTime
+    putStrLn $ "Took " ++ secs (endTime - startTime) ++ " to solve"
+
+-- Algos
+
+-- Primes.  Taken from https://wiki.haskell.org/Prime_numbers#Tree_merging
+_Y   :: (t -> t) -> t
+_Y g = g (_Y g)                -- multistage, non-sharing,  g (g (g (g ...)))
+    -- g (let x = g x in x)    -- two g stages, sharing
+primes = 2 : _Y ((3:) . gaps 5 . joinT . map (\p-> [p*p, p*p+2*p..]))
+
+-- joinL ((x:xs):t) = x : union xs (joinL t)
+joinT ((x:xs):t) = x : union xs (joinT (pairs t))    -- set union, ~=
+  where  pairs (xs:ys:t) = union xs ys : pairs t     --    nub.sort.concat
+
+gaps k s@(x:xs) | k < x = k:gaps (k+2) s       -- ~= [k,k+2..]\\s,
+                | True  =   gaps (k+2) xs      --   when null(s\\[k,k+2..])
